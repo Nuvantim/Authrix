3b0358dab7df0631846f02e4a11d2d2ea5dcc927
Kalveir <71752646+Kalveir@users.noreply.github.com>
2025-07-15 12:42:58 +0700

    Update otp.sql

--- dev/database/init/queries/otp.sql
+++ dev/database/init/queries/otp.sql
@@ -2,7 +2,7 @@
 INSERT INTO otp_token(code,email) VALUES ($1,$2) RETURNING *;
 
 -- name: FindOTP :one
-SELECT COUNT(*) FROM otp_token WHERE code = $1 LIMIT 1;
+SELECT COUNT(*) FROM otp_token WHERE code = $1 AND WHERE email = $2;
 
 -- name: DeleteOTP :exec
 DELETE FROM otp_token WHERE email = $1;
